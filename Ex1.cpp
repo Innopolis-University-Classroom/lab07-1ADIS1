2daa27e43579f49131ecb6dc1ab240442c224fb3
1ADIS1 <60191045+1ADIS1@users.noreply.github.com>
2022-03-04 16:52:36 +0300

    First try

---
+++ Ex1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+template<typename WhatKind>
+void multiples(WhatKind& sum, WhatKind x, int n) {
+    for (int i = 0; i < n; i++) {
+        if (i == 0) {
+            sum += 1;
+        }
+        else {
+            sum += i * x;
+        }
+    }
+}
+
+int main() {
+
+}
