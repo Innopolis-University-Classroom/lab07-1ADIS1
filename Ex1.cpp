3b36e9553ed7cfd7642bacde71c6ea3904429713
1ADIS1 <60191045+1ADIS1@users.noreply.github.com>
2022-03-04 16:56:26 +0300

    Add files via upload

--- Ex1.cpp
+++ Ex1.cpp
@@ -6,13 +6,8 @@ void multiples(WhatKind& sum, WhatKind x, int n) {
     for (int i = 0; i < n; i++) {
         if (i == 0) {
             sum += 1;
-        }
-        else {
+        } else {
             sum += i * x;
         }
     }
-}
-
-int main() {
-
 }
