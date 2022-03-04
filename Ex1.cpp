d7597990ca5338dd8f1b8ad25631e9f72f114013
1ADIS1 <60191045+1ADIS1@users.noreply.github.com>
2022-03-04 17:01:38 +0300

    Add files via upload

--- Ex1.cpp
+++ Ex1.cpp
@@ -3,11 +3,22 @@ using namespace std;
 
 template<typename WhatKind>
 void multiples(WhatKind& sum, WhatKind x, int n) {
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i <= n; i++) {
         if (i == 0) {
             sum += 1;
         } else {
             sum += i * x;
         }
     }
+}
+
+int main() {
+    int y = 0;
+    multiples(y, 1, 2);
+
+    float y2 = 0.0;
+    multiples<float>(y2, 1.5, 3.0);
+
+    cout << y << endl;
+    cout << y2 << endl;
 }
