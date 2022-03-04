4fc75855b79eed9a99ff7488291a55a93d0e38b5
1ADIS1 <60191045+1ADIS1@users.noreply.github.com>
2022-03-04 17:22:42 +0300

    Add files via upload

--- Ex1.cpp
+++ Ex1.cpp
@@ -15,9 +15,10 @@ void multiples(WhatKind& sum, WhatKind x, int n) {
 template<typename T>
 class Calculator
 {
-            T num1;
-            T num2;
 public:
+    T num1;
+    T num2;
+
     Calculator(T num1, T num2) {
         this->num1 = num1;
         this->num2 = num2;
@@ -40,39 +41,37 @@ public:
     }
 };
 
-//int main() {
-//    int y = 0;
-//    multiples(y, 1, 2);
-//
-//    float y2 = 0.0;
-//    multiples<float>(y2, 1.5, 3.0);
-//
-//    cout << y << endl;
-//    cout << y2 << endl;
-//}
+int main() {
+    int y = 0;
+    multiples(y, 1, 2);
 
-//int main() {
-//    Calculator<int> intCalc(2, 1);
-//
-////    REQUIRE(intCalc.add() == 3);
-////    REQUIRE(intCalc.multiply() == 2);
-////    REQUIRE(intCalc.divide() == 2);
-////    REQUIRE(intCalc.subtract() == 1);
-//
-//    cout << intCalc.add() << endl;
-//    cout << intCalc.multiply() << endl;
-//    cout << intCalc.divide() << endl;
-//    cout << intCalc.subtract() << endl;
-//
-//    Calculator<float> floatCalc(12.0, 3.0);
-//
-////    REQUIRE(floatCalc.add() == 15.0);
-////    REQUIRE(floatCalc.multiply() == 36.0);
-////    REQUIRE(floatCalc.divide() == 4.0);
-////    REQUIRE(floatCalc.subtract() == 9.0);
-//
-//    cout << floatCalc.add() << endl;
-//    cout << floatCalc.multiply() << endl;
-//    cout << floatCalc.divide() << endl;
-//    cout << floatCalc.subtract() << endl;
-//}
+    float y2 = 0.0;
+    multiples<float>(y2, 1.5, 3.0);
+
+    cout << y << endl;
+    cout << y2 << endl;
+
+    Calculator<int> intCalc(2, 1);
+
+//    REQUIRE(intCalc.add() == 3);
+//    REQUIRE(intCalc.multiply() == 2);
+//    REQUIRE(intCalc.divide() == 2);
+//    REQUIRE(intCalc.subtract() == 1);
+
+    cout << intCalc.add() << endl;
+    cout << intCalc.multiply() << endl;
+    cout << intCalc.divide() << endl;
+    cout << intCalc.subtract() << endl;
+
+    Calculator<float> floatCalc(12.0, 3.0);
+
+//    REQUIRE(floatCalc.add() == 15.0);
+//    REQUIRE(floatCalc.multiply() == 36.0);
+//    REQUIRE(floatCalc.divide() == 4.0);
+//    REQUIRE(floatCalc.subtract() == 9.0);
+
+    cout << floatCalc.add() << endl;
+    cout << floatCalc.multiply() << endl;
+    cout << floatCalc.divide() << endl;
+    cout << floatCalc.subtract() << endl;
+}
