1038c6e77485b8762a9fe181353cb9381e91a76a
1ADIS1 <60191045+1ADIS1@users.noreply.github.com>
2022-03-04 17:03:53 +0300

    Add files via upload

--- Ex1.cpp
+++ Ex1.cpp
@@ -12,13 +12,13 @@ void multiples(WhatKind& sum, WhatKind x, int n) {
     }
 }
 
-int main() {
-    int y = 0;
-    multiples(y, 1, 2);
-
-    float y2 = 0.0;
-    multiples<float>(y2, 1.5, 3.0);
-
-    cout << y << endl;
-    cout << y2 << endl;
-}
+//int main() {
+//    int y = 0;
+//    multiples(y, 1, 2);
+//
+//    float y2 = 0.0;
+//    multiples<float>(y2, 1.5, 3.0);
+//
+//    cout << y << endl;
+//    cout << y2 << endl;
+//}
